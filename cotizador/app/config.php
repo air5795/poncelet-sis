9869877eb28b28d0f1cfdaf43fefeb208ebd17bb
ale pablo <ale.empresa2.com>
2022-10-30 21:36:33 -0400

    Revert "a"
    This reverts commit 1d6c55def72ec8264335f67f345ab0bde5fe9001.

--- cotizador/app/config.php
+++ cotizador/app/config.php
@@ -5,7 +5,10 @@ session_start();
 //para saber si esta en servidor local 
 
 define('IS_LOCAL',in_array($_SERVER['REMOTE_ADDR'],['12.0.0.1','::1']));
-define('URL',(IS_LOCAL ? 'http://127.0.0.1/poncelet-sis/cotizador/': 'LA URL DE SU SERVIDOR EN PRODUCCION'));
+//define('IS_REMOTE',in_array($_SERVER['REMOTE_HOST'],['192.168.0.10',':80']));
+define('URL',(IS_LOCAL ? 'http://127.0.0.1/poncelet-sis/cotizador/': ''));
+
+
 
 //Rutas para carpetas
 
@@ -30,6 +33,8 @@ define('APP_NAME','Cotizador Poncelet');
 define('TAXES_RATE',0.03);
 define('SHIPPING',0);
 
+//autoload composer
+ require_once ROOT.'vendor/autoload.php';
 
 // cargar las funciones
 
--- cotizador/templates/includes/footer.php
+++ cotizador/templates/includes/footer.php
@@ -21,6 +21,8 @@
 
     <script src="<?php echo JS.'waitMe.min.js'?>"></script>
     <script src="<?php echo JS.'main.js'?>"></script>
+
+
   </body>
 </html>
 
